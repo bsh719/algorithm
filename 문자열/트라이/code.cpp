b3f84cd874e6b25805d99a59449c6f4886848ec5
Siha Baek <123621949+bsh2023@users.noreply.github.com>
2023-01-26 20:38:37 +0900

    Create code.cpp

---
+++ 문자열/트라이/code.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int trie[100000][26], cnt[100000];
+
+int main() {
+	int N; cin >> N;
+	int last = 0;
+
+	for (int i = 0; i < N; i++) {
+		string st; cin >> st;
+		int now = 0;
+
+		for (char ch : st) {
+			int t = ch - 'a';
+			if (trie[now][t] == 0) trie[now][t] = ++last;
+			now = trie[now][t];
+		}
+    cnt[now]++;
+	}
+}
