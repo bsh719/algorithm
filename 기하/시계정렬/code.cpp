145a119f136a7ce4d1857e9bca54d1fd84917c52
Siha Baek <123621949+bsh0719@users.noreply.github.com>
2023-01-26 21:40:16 +0900

    Update code.cpp

--- 기하/시계정렬/code.cpp
+++ 기하/시계정렬/code.cpp
@@ -1,1 +1,22 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
 
+# sort(Pr, Pr + N, cmp)로 정렬
+
+bool cmp(int i, int j) {
+	int n1, n2;
+	if (X[i] >= 0 && Y[i] >= 0) n1 = 1;
+	else if (X[i] <= 0 && Y[i] >= 0) n1 = 4;
+	else if (X[i] <= 0 && Y[i] <= 0) n1 = 3;
+	else n1 = 2;
+
+	if (X[j] >= 0 && Y[j] >= 0) n2 = 1;
+	else if (X[j] <= 0 && Y[j] >= 0) n2 = 4;
+	else if (X[j] <= 0 && Y[j] <= 0) n2 = 3;
+	else n2 = 2;
+
+	if (n1 != n2) return n1 < n2;
+
+	return Y[i] * X[j] > Y[j] * X[i];
+}
